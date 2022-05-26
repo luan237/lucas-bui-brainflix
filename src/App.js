ec804ce1f4c012b85a717d952d2aa0128c12ab13
lucas-bui <luan237@gmail.com>
2022-05-26 17:39:04 -0400

    continue with video details

--- src/App.js
+++ src/App.js
@@ -1,7 +1,5 @@
 import "./App.scss";
 import Header from "./components/Header/Header.js";
-import Video from "./components/Video/Video.js";
-import VideoDetails from "./components/VideoDetails/VideoDetails.js";
 import Main from "./components/Main/Main.js";
 
 function App() {
