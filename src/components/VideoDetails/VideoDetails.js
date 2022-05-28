ced7621c6739e163552cd12be1ecb6a27dfb988f
Lucas <luan237@gmail.com>
2022-05-28 16:56:30 -0400

    Video details section

--- src/components/VideoDetails/VideoDetails.js
+++ src/components/VideoDetails/VideoDetails.js
@@ -1,11 +1,24 @@
 import "./VideoDetails.scss";
+import viewIcon from "../../assets/icons/views.svg";
+import likesIcon from "../../assets/icons/likes.svg";
 
 function VideoDetails(props) {
+  let newTime = new Date(props.showingVideo.timestamp).toLocaleDateString();
   return (
-    <div className="video__detail">
-      <div className="video__detail--left">
-        <p className="left__author">By {props.showingVideo.channel}</p>
-        <p className="left__date">{props.showingVideo.timestamp}</p>
+    <div className="detail">
+      <div className="detail__left">
+        <p className="detail__left--author">By {props.showingVideo.channel}</p>
+        <p className="detail__left--date">{newTime}</p>
+      </div>
+      <div className="detail__right">
+        <div className="detail__right--views">
+          <img src={viewIcon} alt="views icon" />
+          <p>{props.showingVideo.views}</p>
+        </div>
+        <div className="detail__right--likes">
+          <img src={likesIcon} alt="likes icon" />
+          <p>{props.showingVideo.likes}</p>
+        </div>
       </div>
     </div>
   );
