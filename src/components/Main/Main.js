683e585dd8a7386fd5f0e967dd45e038fa5834b0
Lucas <luan237@gmail.com>
2022-05-29 22:11:00 -0400

    modified video basic list

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -4,30 +4,33 @@ import VideoDetails from "../VideoDetails/VideoDetails.js";
 import VideoList from "../VideoList/VideoList.js";
 import React from "react";
 import VideoDetailsList from "../../data/video-details.json";
-import VideosID from "../../data/videos.json";
+import VideosBasic from "../../data/videos.json";
 import Comments from "../Comments/Comments";
 
-const videosID = Array.from(VideosID);
+const videosBasic = Array.from(VideosBasic);
 const videoDetailsList = Array.from(VideoDetailsList);
 
 class Main extends React.Component {
   state = {
-    videosID,
+    videosBasic,
     videoDetailsList,
-    showingVideo: videoDetailsList[0],
+    showingVideo: videosBasic[0],
+    showingVideoDetails: videoDetailsList[0],
   };
 
   updatedVideo = (id) => {
-    let showingVideo2 = this.state.videoDetailsList.find(
+    let showingVideo2 = this.state.videosBasic.find((video) => video.id === id);
+    let showingVideoDetails2 = this.state.videoDetailsList.find(
       (video) => video.id === id
     );
     this.setState({
       showingVideo: showingVideo2,
+      showingVideoDetails: showingVideoDetails2,
     });
   };
 
   updatedVideoList = () => {
-    let updatedList = [...VideoDetailsList];
+    let updatedList = [...VideosBasic];
     let showingIndex = updatedList.indexOf(this.state.showingVideo);
     updatedList.splice(showingIndex, 1);
     return updatedList;
@@ -38,8 +41,10 @@ class Main extends React.Component {
         <Video showingVideo={this.state.showingVideo} />
         <div className="page">
           <div className="page__left">
-            <VideoDetails showingVideo={this.state.showingVideo} />
-            <Comments showingComments={this.state.showingVideo.comments} />
+            <VideoDetails showingVideo={this.state.showingVideoDetails} />
+            <Comments
+              showingComments={this.state.showingVideoDetails.comments}
+            />
           </div>
           <VideoList
             newList={this.updatedVideoList()}
